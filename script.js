5f65e27528bb651e335285419144a5b9d38d111b
Larissa Giotti <61012885+larissagiotti@users.noreply.github.com>
2022-10-06 11:39:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("botao-enviar").addEventListener("click",validaFormulario)
+
+function validaFormulario(){
+  if (document.getElementById("nome").value != "" && document.getElementById("email").value != ""  ){
+  alert("Prontinho! você receberá as novidades por e-mail")
+  }else{
+    alert("Por favor, preencha os campos nome e email!")
+} 
+}
